fd02ca4270410b600270442576aff9f6599ce2e0
Leong He <leongh@mozy.com>
2013-05-24 14:55:16 +0800

    Fix broken cases due to missing space
    Change-Id: Ib994ae0542093e2ad0e1bca06dbf155f4cae89fd
    Reviewed-on: https://gerrit.dechocorp.com/51546
    Reviewed-by: Congshan Liu <congshanl@mozy.com>

--- features/step_definitions/users/add_edit_user_group_steps.rb
+++ features/step_definitions/users/add_edit_user_group_steps.rb
@@ -60,7 +60,7 @@ Then /^(.+) user group should be (created|updated)$/ do |ug, action|
 
   case action
     when 'created'
-      @bus_site.admin_console_page.add_new_user_group_section.success_messages.should == "User Group #{group_name}has been successfully created."
+      @bus_site.admin_console_page.add_new_user_group_section.success_messages.should == "User Group #{group_name} has been successfully created."
       # Clear previous message
       @bus_site.admin_console_page.add_new_user_group_section.refresh_bus_section
       @bus_site.admin_console_page.add_new_user_group_section.wait_until_bus_section_load
