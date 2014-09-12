0964efff6e28d5bbf343a9e5e900de17f926436c
Catherine Liao <catherinel@mozy.com>
2014-09-12 15:29:58 +0800

    fix test cases 12185 by modify css to xpath, refs #124463 Tested in BUS build 2.14.0.12
    Signed-off-by: Catherine Liao <catherinel@mozy.com>
    Change-Id: I987346f50d8ba27b6bf5a1962036f92bc53a535a
    Reviewed-on: https://gerrit.dechocorp.com/74477
    Reviewed-by: Zeng Jiani <Zoe.Zeng@emc.com>

--- test_sites/bus_site/sections/resources/data_shuttle/data_shuttle_status_section.rb
+++ test_sites/bus_site/sections/resources/data_shuttle/data_shuttle_status_section.rb
@@ -5,7 +5,7 @@ module Bus
     # Private elements
     #
     element(:device_status_table, xpath: "//table[@class='mini-table']/thead[1]")
-    element(:device_stuck_table, css: 'table.mini-table:nth-child(2)')
+    element(:device_stuck_table, xpath: "//table[@class='mini-table'][2]")
     element(:inventory_status_table, css: 'table.mini-table:last-child')
 
     # Device status tables
