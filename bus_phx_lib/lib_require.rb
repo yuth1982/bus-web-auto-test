0016f6ba1b6bfeeb768ad291107ee490d901830d
Jason Mavandi <jason.mavandi@emc.com>
2014-10-21 10:51:10 -0600

    Revert "Fix lib for windows"
    This reverts commit de90213245ab6b958c9a6ab1f7310427df3e5786.
    
    Change-Id: I6c31a20802844b582c1026cf34263b5e50605e02
    Reviewed-on: https://gerrit.dechocorp.com/76317
    Reviewed-by: Jason Mavandi <jason.mavandi@emc.com>

--- bus_phx_lib/lib_require.rb
+++ bus_phx_lib/lib_require.rb
@@ -1,7 +1,7 @@
 #This requires all the methods to use instead of cucumber
 
 def require_all_files_below(folder)
-  Dir["#{folder}/**/*.rb"].each {|rb_file| require rb_file}
+  Dir["/#{folder}/**/*.rb"].each {|rb_file| require rb_file}
 end
 methods_folder = File.join(File.expand_path(File.dirname(__FILE__)),"lib_methods")
 require_all_files_below(methods_folder)
