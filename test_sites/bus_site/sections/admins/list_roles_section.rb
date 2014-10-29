ac4971b469d0a52db9dd11e6fa75d39074532f5f
lily.cui <lily.cui2@emc.com>
2014-10-29 14:50:32 +0800

    update list_role function to find the role page by page, not only on the first page
    Change-Id: Ia4adc26c5d64dffc184d454cf1bfe01a62978ac0
    Reviewed-on: https://gerrit.dechocorp.com/76653
    Reviewed-by: Catherine Liao <catherinel@mozy.com>

--- test_sites/bus_site/sections/admins/list_roles_section.rb
+++ test_sites/bus_site/sections/admins/list_roles_section.rb
@@ -3,7 +3,15 @@ module Bus
   class ListRolesSection < SiteHelper::Section
 
     def list_role(role_name)
-      find(:xpath, "//a[text() = '#{role_name}']").click
+      multi_page_link = "//div[@class='table-metadata']//p[3]//a"
+      role_name_link = "//a[text()='#{role_name}']"
+      index = 1
+      while (all(:xpath, role_name_link).size == 0)
+        find(:xpath, multi_page_link+"[#{index}]").click
+        wait_until_bus_section_load
+        index += 1
+      end
+      find(:xpath, role_name_link).click
     end
 
     def all_role_name_started_with(name)
