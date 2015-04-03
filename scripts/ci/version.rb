fe201bdd6d0405af0cd6d4693fe492b2be27b911
liny14 <you.lin@emc.com>
2015-04-03 16:42:01 +0800

    move ci fold into script folder and fix get_version issue
    Change-Id: I84c15378e863104006c3a3ef72e0930f504efe51
    Reviewed-on: https://gerrit.dechocorp.com/84365
    Reviewed-by: Lin You <you.lin@emc.com>

--- scripts/ci/version.rb
+++ scripts/ci/version.rb
@@ -5,7 +5,7 @@ module Version
   def get_version
     file = nil
     begin
-      res = post('https://www.mozypro.com/version.txt')
+      res = HTTParty.post('https://www.mozypro.com/version.txt')
       version = res.to_s
       if version.end_with?("\n")
         version = version[0, version.length-1]
@@ -13,7 +13,7 @@ module Version
       file = File.new("version.#{version}", 'w')
       file.puts version
     rescue Exception => ex
-      Log.debug(ex.to_s)
+      puts ex.to_s
     ensure
       file.close if !file.nil?
     end
