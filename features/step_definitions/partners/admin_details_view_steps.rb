5a4ea8f6c206353f5b10199b26f22de846ce0706
Amy Jiang <Amy.Jiang@emc.com>
2015-08-31 23:29:52 -0600

    correct a step
    Change-Id: I6ae4de724d90111c7c2bbcdf3982122c4ec91639
    Reviewed-on: https://gerrit.mozy.lab.emc.com/90629
    Reviewed-by: Amy Jiang <Amy.Jiang@emc.com>

--- features/step_definitions/partners/admin_details_view_steps.rb
+++ features/step_definitions/partners/admin_details_view_steps.rb
@@ -7,7 +7,7 @@
 #   fill in confirm password with default password
 #   click 'Save Changes'
 #
-When /^I activate new partner admin with (default password|Hipaa password)$/ do |password_type|
+When /^I activate new partner admin with (default password|Hipaa password)$/ do |password|
   @bus_site.admin_console_page.navigate_to_menu(CONFIGS['bus']['menu']['search_list_partner'])
   @bus_site.admin_console_page.search_list_partner_section.search_partner(@partner.admin_info.email)
   @bus_site.admin_console_page.search_list_partner_section.view_partner_detail(@partner.admin_info.email)
