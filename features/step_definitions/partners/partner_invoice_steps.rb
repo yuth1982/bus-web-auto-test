1005a16bb0363c2293a6415346e34a651db0d1c2
hongyc <Catherine.HongyanLiao@mozy.com>
2016-04-26 13:41:43 +0800

    update invoice date which should without zero-padded
    Signed-off-by: hongyc <Catherine.HongyanLiao@mozy.com>
    Change-Id: I6f624cd2bf00c6debc590afffeb3641c6b2662d5
    Reviewed-on: https://gerrit.mozy.lab.emc.com/99429

--- features/step_definitions/partners/partner_invoice_steps.rb
+++ features/step_definitions/partners/partner_invoice_steps.rb
@@ -9,12 +9,12 @@ And /^Billing details of partner invoice should be:$/ do |billing_detail_table|
   expected_arr.each_index { |i|
     Log.debug expected_arr
     with_timezone(ARIA_ENV['timezone']) do
-      expected_arr[i][0].replace(Chronic.parse(expected_arr[i][0]).strftime('%m/%d/%Y')) unless expected_arr[i][0].size == 0
+      expected_arr[i][0].replace(Chronic.parse(expected_arr[i][0]).strftime('%-m/%-d/%Y')) unless expected_arr[i][0].size == 0
       if expected_arr[i][1] == 'after 1 year yesterday'
         date = Chronic.parse("after 1 year")
         expected_arr[i][1] = date.month.to_s+"/"+(date.day-1).to_s+"/"+date.year.to_s
       else
-        expected_arr[i][1].replace(Chronic.parse(expected_arr[i][1]).strftime('%m/%d/%Y')) unless expected_arr[i][1].size == 0
+        expected_arr[i][1].replace(Chronic.parse(expected_arr[i][1]).strftime('%-m/%-d/%Y')) unless expected_arr[i][1].size == 0
       end
     end
   }
