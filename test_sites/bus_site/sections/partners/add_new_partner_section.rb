759f637d2c5ce4d34dffc8c3bc028d8498fc30a3
zengz2 <Zoe.Zeng@emc.com>
2016-08-17 17:18:33 +0800

    Correct click create_partner_btn, not click button again if it is clicked or partner already created
    Change-Id: Ic366073e6430d9944448dceee93f15b67c437982
    Reviewed-on: https://gerrit.mozy.lab.emc.com/104299
    Reviewed-by: Zoe Zeng <Zoe.Zeng@emc.com>

--- test_sites/bus_site/sections/partners/add_new_partner_section.rb
+++ test_sites/bus_site/sections/partners/add_new_partner_section.rb
@@ -121,7 +121,7 @@ module Bus
         #Pause for 3 seconds as a workaround
         #sleep 3
         5.times do
-          create_partner_btn.click if !locate(:css, "input[value='Create Partner']").nil?
+          create_partner_btn.click if !locate(:css, "input[value='Create Partner']").nil? && create_partner_btn.visible? && create_partner_btn['disabled'] != 'true'
           sleep 5
           break if !locate(:css, "div#partner-new-errors ul").nil?
         end
