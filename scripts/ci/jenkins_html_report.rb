b277fb481d7fb9cd15d1f793163c85785ae31a2f
hongyc <Catherine.HongyanLiao@emc.com>
2017-05-26 17:20:26 +0800

    fix when there is issue with before build
    Signed-off-by: hongyc <Catherine.HongyanLiao@emc.com>
    Change-Id: I86b770c5f6f80b015ebad6df80b47830d7da3460
    Reviewed-on: https://gerrit.mozy.lab.emc.com/113185
    Reviewed-by: Jaden Zhang <Jaden.Zhang@emc.com>

--- scripts/ci/jenkins_html_report.rb
+++ scripts/ci/jenkins_html_report.rb
@@ -5,6 +5,13 @@ require 'nokogiri'
 require 'open-uri'
 require 'net/https'
 
+opts = GetoptLong.new(
+    [ "--help",            "-h", GetoptLong::NO_ARGUMENT       ],
+    [ "--job_name",        "-n", GetoptLong::REQUIRED_ARGUMENT ],
+    [ "--build_id",        "-i", GetoptLong::OPTIONAL_ARGUMENT ],
+    [ "--jenkins",         "-j", GetoptLong::OPTIONAL_ARGUMENT ]
+)
+
 help = false
 job_name = "bus_smoke_test_qa6.busclient01"
 build_id = 1
@@ -15,13 +22,6 @@ total = 0
 passed = 0
 failed = 0
 
-opts = GetoptLong.new(
-    [ "--help",            "-h", GetoptLong::NO_ARGUMENT       ],
-    [ "--job_name",        "-n", GetoptLong::REQUIRED_ARGUMENT ],
-    [ "--build_id",        "-i", GetoptLong::REQUIRED_ARGUMENT ],
-    [ "--jenkins",         "-j", GetoptLong::NO_ARGUMENT       ]
-)
-
 opts.each do |opt, arg|
   case opt
     when '--help'
@@ -30,7 +30,7 @@ usage
   `ruby scripts/ci/jenkins_html_report.rb --job_name "bus_smoke_test_qa6.busclient01" --build_id 3`
   `ruby scripts/ci/jenkins_html_report.rb -n "bus_smoke_test_qa6.busclient01" -i 3`
   `ruby scripts/ci/jenkins_html_report.rb --job_name "bus_smoke_test_qa6.busclient05" --build_id lastCompletedBuild`
-  `ruby scripts/ci/jenkins_html_report.rb --job_name "bus_smoke_test_qa6.busclient05" --build_id lastCompletedBuild --jenkins`
+  `ruby scripts/ci/jenkins_html_report.rb -n "bus_smoke_test_qa6.busclient05" -j`
       EOF
       help = true
       break
@@ -53,21 +53,19 @@ Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_m
   build_number = response.body
 end
 
-doc = Nokogiri::HTML(open("http://jenkins01.tools.mozyops.com/view/apps-automation/job/#{job_name}/#{build_id}/cucumber-html-reports/overview-features.html", http_basic_authentication: ["hongyc", "QAP@SSw0rd1!"]))
-
 if jenkins
-  doc.xpath("//table[@id='build-info']/tbody/tr/td[2]").each do |link|
-    build_id = link.content
-    build_id = build_id.to_i + 1
-    build_id.to_s
+  doc = Nokogiri::HTML(open("http://jenkins01.tools.mozyops.com/job/#{job_name}/", http_basic_authentication: ["hongyc", "QAP@SSw0rd1!"]))
+  doc.xpath("//img[@tooltip='In progress > Console Output']/../../../a").each do |link|
+    build_id = link.content.match(/\d+/)
   end
-elsif build_id == 'lastCompletedBuild'
+else
+  doc = Nokogiri::HTML(open("http://jenkins01.tools.mozyops.com/job/#{job_name}/#{build_id}/cucumber-html-reports/overview-features.html", http_basic_authentication: ["hongyc", "QAP@SSw0rd1!"]))
   doc.xpath("//table[@id='build-info']/tbody/tr/td[2]").each do |link|
     build_id = link.content
   end
 end
 
-doc = Nokogiri::HTML(open("http://jenkins01.tools.mozyops.com/view/apps-automation/job/#{job_name}/#{build_id}/cucumber-html-reports/overview-features.html", http_basic_authentication: ["hongyc", "QAP@SSw0rd1!"]))
+doc = Nokogiri::HTML(open("http://jenkins01.tools.mozyops.com/job/#{job_name}/#{build_id}/cucumber-html-reports/overview-features.html", http_basic_authentication: ["hongyc", "QAP@SSw0rd1!"]))
 
 doc.xpath("//table[@id='tablesorter']//tfoot/tr[1]/td[10]").each do |link|
   total = link.content
